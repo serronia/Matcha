6ca8c80814f6a2ce1061a5f2e4a1b8ef01d61e69
Marie Chapard <mchapard@z2r1p1.le-101.fr>
2019-01-09 14:24:03 +0100

    first commit

---
+++ app/src/app.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+
+var server = http.createServer(function(req, res) {
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  var page = url.parse(req.url).pathname;
+  console.log(page);
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.write('<!DOCTYPE html>'+
+    '<html>'+
+    '    <head>'+
+    '        <meta charset="utf-8" />'+
+    '        <title>Ma page Node.js !</title>'+
+    '    </head>'+ 
+    '    <body style="display: flex;justify-content: center;">');
+  if (page == '/')
+  {
+    res.write('<p>Vous êtes à l\'accueil, que puis-je pour vous ?<p>');
+  }
+  else if (page == '/sous-sol')
+  {
+      res.write('<p>Vous êtes dans la cave à vins, ces bouteilles sont à moi !<p>');
+  }
+  else if (page == '/etage/1/chambre')
+  {
+      res.write('<p>Hé ho, c\'est privé ici !<p>');
+      var params = querystring.parse(url.parse(req.url).query);
+      if ('prenom' in params && 'nom' in params) {
+          res.write('Vous vous appelez ' + params['prenom'] + ' ' + params['nom']);
+      }
+      else {
+          res.write('Vous devez bien avoir un prénom et un nom, non ?');
+      }
+  }
+  else
+  {
+    res.write('<p style="color: red;">404 erreur page not found</p>');
+  }
+  res.write('</body>'+
+'</html>');
+  res.end();
+});
+server.listen(8080);
