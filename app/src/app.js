452cb52f206887680dd4033d4164a0c259fcbeb9
Marie Chapard <mchapard@z2r3p2.le-101.fr>
2019-01-11 21:00:17 +0100

    lol2

--- app/src/app.js
+++ app/src/app.js
@@ -19,6 +19,7 @@ var server = http.createServer(function(req, res) {
     '    <head>'+
     '        <meta charset="utf-8" />'+
     '        <title>Ma page Node.js !</title>'+
+    '        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">'+
     '    </head>'+ 
     '    <body style="display: flex;">');//justify-content: center;
   if (page == '/')
