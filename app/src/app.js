f17198af55bcced5379aa9c3f9f5fae7495fb00f
Marie Chapard <mchapard@z2r1p3.le-101.fr>
2019-01-15 20:09:02 +0100

    miou

--- app/src/app.js
+++ app/src/app.js
@@ -20,6 +20,7 @@ var server = http.createServer(function(req, res) {
     '        <meta charset="utf-8" />'+
     '        <title>Ma page Node.js !</title>'+
     '        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">'+
+    '        <link rel="stylesheet" href="bla.css">'+
     '    </head>'+ 
     '    <body style="display: flex;">');//justify-content: center;
   if (page == '/')
