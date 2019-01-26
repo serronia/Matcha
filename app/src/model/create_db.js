9972f16bc95ada7ad6a3ce78568e1aa577dcbe43
jdarko <jevtic.darko.3@gmail.com>
2019-01-26 17:28:41 +0100

    ytre

--- app/src/model/create_db.js
+++ app/src/model/create_db.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+/*
 var mysql = require('mysql');
 var database_var = process.env.DB_NAME;
 var con = mysql.createConnection(
@@ -10,20 +10,18 @@ var con = mysql.createConnection(
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
     });
+*/
 
-
-
+var con = require('../db');
 
 
 router.get('/', function(req, res)
 {
-
-
   con.connect(function(err) 
   {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    var sql = "CREATE TABLE IF NOT EXIST customers (name VARCHAR(255), address VARCHAR(255))";
     con.query(sql, function (err, result) 
     {
       if (err) throw err;
