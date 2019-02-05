0ccf479f29f1ec304862678acbf1cc6982ecffac
jdarko <jevtic.darko.3@gmail.com>
2019-02-05 21:45:16 +0100

    push jdarko for bdd

--- app/src/model/create_db.js
+++ app/src/model/create_db.js
@@ -23,13 +23,43 @@ router.get('/db', function(req, res)
     con.query(sql, function (err, result) 
     {
       if (err) throw err;
-      console.log("OK!");
+      console.log("[SQL][TABLE] \"utilsateur\" exist or created!");
     });
     var sql = "CREATE TABLE IF NOT EXISTS preference (id INT(255) not null auto_increment primary key, genre ENUM('hommme', 'femme') not null, orientation ENUM('homme', 'femme', 'bi') not null, bio VARCHAR(255) not null, latitude DOUBLE(255, 25) not null, longitude DOUBLE(255, 25) not null, tag VARCHAR(255) not null)";
     con.query(sql, function (err, result) 
     {
       if (err) throw err;
-      console.log("OK!");
+      console.log("[SQL][TABLE] \"preference\" exist or created!");
+    });
+    var sql = "CREATE TABLE IF NOT EXISTS photo (id INT(255) not null auto_increment primary key, id_photo_profile INT(255) not null, photo_1 VARCHAR(255) not null, photo_2 VARCHAR(255), photo_3 VARCHAR(255), photo_4 VARCHAR(255), photo_5 VARCHAR(255))";
+    con.query(sql, function (err, result) 
+    {
+      if (err) throw err;
+      console.log("[SQL][TABLE] \"photo\" exist or created!");
+    });
+    var sql = "CREATE TABLE IF NOT EXISTS likes (id INT(255) not null auto_increment primary key, id_user_1 INT(255) not null, id_user_2 INT(255) not null)";
+    con.query(sql, function (err, result) 
+    {
+      if (err) throw err;
+      console.log("[SQL][TABLE] \"likes\" exist or created!");
+    });
+    var sql = "CREATE TABLE IF NOT EXISTS chat (id INT(255) not null auto_increment primary key, id_user_1 INT(255) not null, id_user_2 INT(255) not null, id_echange INT(255) not null)";
+    con.query(sql, function (err, result) 
+    {
+      if (err) throw err;
+      console.log("[SQL][TABLE] \"chat\" exist or created!");
+    });
+    var sql = "CREATE TABLE IF NOT EXISTS echange (id INT(255) not null auto_increment primary key, message VARCHAR(255) not null, date datetime not null, user_id INT(255) not null)";
+    con.query(sql, function (err, result) 
+    {
+      if (err) throw err;
+      console.log("[SQL][TABLE] \"echange\" exist or created!");
+    });
+    var sql = "CREATE TABLE IF NOT EXISTS tags (id INT(255) not null auto_increment primary key, name VARCHAR(255) not null)";
+    con.query(sql, function (err, result) 
+    {
+      if (err) throw err;
+      console.log("[SQL][TABLE] \"tags\" exist or created!");
     });
     res.redirect('localhost:8080');
 });
